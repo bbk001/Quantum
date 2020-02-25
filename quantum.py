5bf5d9a7fd9269720dd7646e86616e16343fc8b4
Benjamin Khoury <benjaminbrookskhoury@gmail.com>
2020-02-25 13:35:51 -0800

    Update quantum.py

--- quantum.py
+++ quantum.py
@@ -1,16 +1,16 @@
 import matplotlib.pyplot as plt
 
 def numIntegration(epsilon):
-    delta=0.0001
-    psi0=1
-    psi1=1-delta**2*epsilon
-    psi=[psi0,psi1]
-    for i in range(100000):
-        psi2=-psi0+2*psi1-2*delta**2*(epsilon-abs(i*delta))*psi1
-        psi0=psi1
+    delta=0.0001 #timestep
+    psi0=1 #initial psi
+    psi1=1-delta**2*epsilon #psi_1
+    psi=[psi0,psi1] #array keeps values of psi
+    for i in range(100000): #loops through to find psi_j
+        psi2=-psi0+2*psi1-2*delta**2*(epsilon-abs(i*delta))*psi1 #applies recursive formula
+        psi0=psi1 #redefines psi's to run loop again
         psi1=psi2
-        psi+=[psi1]
-    plt.plot(psi)
+        psi+=[psi1] #stores s=psi_j to array
+    plt.plot(psi) #plots graph to determine whether function diverges up or down
     plt.show()
 
 
